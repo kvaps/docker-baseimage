d4640204b051582f6e83ebd75a0c3782acc53fc4
kvaps <kvapss@gmail.com>
2016-07-12 13:30:12 +0300

    add: simple version

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,7 @@ MAINTAINER kvaps <kvapss@gmail.com>
 
 RUN yum -y install epel-release \
  && yum -y update \
- && yum -y install supervisor cronie rsyslog \
  && yum clean all
 
 ADD bin/* /bin/
 ADD etc/* /etc/
-ADD etc/service /etc/service
--- start.sh
+++ start.sh
@@ -9,14 +9,8 @@ source load_envs
 # Load directories
 source load_dirs
 
-# Load services
-source load_services
-
 # Install updates if neded
 install_updates
 
 # Enable output from logfile
 #tail -f -n0 /var/log/messages | grep ' rsyslogd: ' &
-
-# Start services
-supervisord
