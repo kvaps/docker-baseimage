065bd8fc7fc7be98b150118cc824c0deeb769c31
kvaps <kvapss@gmail.com>
2016-11-26 00:41:26 +0300

    upd: start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,22 @@
 # Load default environment variables
 image_env || exit 1
 
-# Install updates if neded
-if [ -f '/etc/image/version.conf' ]; then
-    image_update || exit 1
-fi
-
 # Conncet directories and files
 image_stor || exit 1
+
+if [ -f '/etc/image/version.conf' ]; then
+    # Install updates if neded
+    image_update || exit 1
+else
+    # First run instructions here
+    #first_run_script || exit 1
+
+    # Write config version
+    echo "0" > /etc/image/version.conf
+fi
 
 # Start services
 image_services_start || exit 1
 
-# Enable output from logfile and hold
-#journalctl -u nginx.service -f
-journalctl -f
+# Attach to syslog and hold
+journalctl -b -ef
